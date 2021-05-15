a6d18780f2dba19cbb41c8d6bb70f885bee85471
sramxtr <roger@moussal.li>
2021-05-15 06:45:02 -0400

    debouncer and respective tb code

--- tutorials/tutorial_2_fsm_and_sim/pattern_checker/sources/design/debouncer.sv
+++ tutorials/tutorial_2_fsm_and_sim/pattern_checker/sources/design/debouncer.sv
@@ -1,10 +1,26 @@
 
-module debouncer(
+module debouncer
+    #(parameter COUNTER_BITS = 21)
+    (
     input logic clk,
-    input logic button_in,
-    output logic button_out
+    input logic data_in,
+    output logic data_out
     );
+        
+    logic data_ff1_postbuf_sig;
+    logic data_ff2_postbuf_sig;
+    logic dataout_prebuf_sig, dataout_postbuf_sig = 0;
+    logic [COUNTER_BITS-1:0] counter_prebuf_sig, counter_postbuf_sig = 0;
     
-    // TODO
+    assign data_out = dataout_postbuf_sig;
+    assign counter_prebuf_sig = (data_ff1_postbuf_sig == data_ff2_postbuf_sig) ? counter_postbuf_sig + 1 : 0;
+    assign dataout_prebuf_sig = (counter_postbuf_sig == '1) ? data_ff2_postbuf_sig : dataout_postbuf_sig;
+
+    always_ff @ (posedge clk) begin
+        data_ff1_postbuf_sig <= data_in;
+        data_ff2_postbuf_sig <= data_ff1_postbuf_sig;
+        counter_postbuf_sig <= counter_prebuf_sig;
+        dataout_postbuf_sig <= dataout_prebuf_sig;
+    end // always_ff
     
 endmodule
---
+++ tutorials/tutorial_2_fsm_and_sim/pattern_checker/sources/simulation/debouncer_tb.sv
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+
+module debouncer_tb(
+    );
+    
+    logic clk_sig = 0;
+    logic data_in_sig, data_out_sig;
+    
+    always begin
+        #0.5 clk_sig <= ~clk_sig;
+    end
+    
+    initial begin
+        data_in_sig = 0; #10
+        assert(data_out_sig == 0) else $error("Hmm.. why is data_out not 0??");
+        
+        // Fluctuate then stabilize at 1
+        // Each fluctuation lasts less than 2^COUNTER_BITS cycles
+        data_in_sig = 1; #2
+        assert(data_out_sig == 0);
+        data_in_sig = 0; #3
+        data_in_sig = 1; #1
+        assert(data_out_sig == 0);
+        data_in_sig = 0; #6
+        // Stabilize at 1
+        data_in_sig = 1; #10
+        assert(data_out_sig == 1) else $error("Expected data_out to be 1 by now");
+        
+        // Fluctuate then stabilize at 0
+        data_in_sig = 0; #1
+        assert(data_out_sig == 1);
+        data_in_sig = 1; #3
+        data_in_sig = 0; #5
+        assert(data_out_sig == 1);
+        data_in_sig = 1; #3
+        // Stabilize at 0
+        data_in_sig = 0; #9
+        assert(data_out_sig == 0) else $error("Expected data_out to be 0 by now");
+        
+        $finish;
+        
+    end // initial
+    
+    // Instantiate the debouncer module
+    debouncer #(.COUNTER_BITS(3)) UUT (.clk(clk_sig),
+                                       .data_in(data_in_sig),
+                                       .data_out(data_out_sig)
+                                       );
+    
+endmodule
